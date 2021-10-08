da56c48a2c58b9d2f4407765911bf4486252bc79
Allerson <allerson.santana354@gmail.com>
2021-10-08 16:45:17 -0300

    Add migration

---
+++ Anagrama.Api/Migrations/20211008184716_CriandoMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Anagrama.Api.Migrations
+{
+    public partial class CriandoMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Anagrama",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Palavra = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Anagrama", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Anagrama");
+        }
+    }
+}
